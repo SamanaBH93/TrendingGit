923d557c01d636e27a1fc8be022397b666315afd
Samana <samanarizvi14@gmail.com>
2022-10-16 18:15:44 +0500

    Create project

---
+++ TrendingGit/TrendingGit/View/TrendingGitTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TrendingGitTableViewController.swift
+//  TrendingGit
+//
+//  Created by VenD-Samana on 16/10/2022.
+//
+
+import UIKit
+
+class TrendingGitTableViewController: UITableViewController {
+    static let identifier = "TrendingGitTableViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
